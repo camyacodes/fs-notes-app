4dff403c38b8b3793c5db4cf35ce07fbc35ee7ca
Camya Robinson <camyacodes@gmail.com>
2024-07-10 23:51:09 -0400

    Added more tests

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -1,15 +1,50 @@
 const { test, after } = require('node:test')
+const Note = require('../models/note')
+const assert = require('assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 
 const api = supertest(app)
 
+const initialNotes = [
+  {
+    content: 'HTML is easy',
+    important: false,
+  },
+  {
+    content: 'Browser can execute only JavaScript',
+    important: true,
+  },
+]
+
+const loadData = async () => {
+  let noteObject = new Note(initialNotes[0])
+  await noteObject.save()
+  noteObject = new Note(initialNotes[1])
+  await noteObject.save()
+}
+
+loadData()
+
 test('notes are returned as json', async () => {
   await api
     .get('/api/notes')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test('there are two notes', async () => {
+  const response = await api.get('/api/notes')
+
+  assert.strictEqual(response.body.length, initialNotes.length)
+})
+
+test('the first note is about HTTP methods', async () => {
+  const response = await api.get('/api/notes')
+
+  const contents = response.body.map((e) => e.content)
+  assert(contents.includes('HTML is easy'))
 })
 
 after(async () => {
